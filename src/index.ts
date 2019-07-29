2041349a12127624b248bcabcd7a437ef6e778c5
Yosef Yuan <yuanruixiangjet@163.com>
2019-07-29 22:12:09 +0800

    function overloading added

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,18 @@ function add7(x: number, ...rest: number[]) {
 }
 
 console.log(add7(1, 2, 3, 4, 5));
+
+function add8(...rest: number[]): number;
+function add8(...rest: string[]): string;
+function add8(...rest: any[]) {
+  let first = rest[0];
+  if (typeof first === "string") {
+    return rest.join("");
+  }
+  if (typeof first === "number") {
+    return rest.reduce((pre, cur) => pre + cur);
+  }
+}
+
+console.log(add8(1, 2, 3));
+console.log(add8("a", "b", "c"));
