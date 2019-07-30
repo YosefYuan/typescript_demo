69ca9f9c60f28bf771e9f103834409c2610bb5b0
Yosef Yuan <yuanruixiangjet@163.com>
2019-07-30 23:11:59 +0800

    ch14 Generics class and Generics constraint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-function log<T>(value: T): T {
-  console.log(value);
+class Log<T> {
+  run(value: T) {
+    console.log(value);
+    return value;
+  }
+}
+
+let log1 = new Log<number>();
+
+log1.run(1);
+
+let log2 = new Log();
+log2.run({ a: 1 });
+log2.run("1");
+
+interface Length {
+  length: number;
+}
+function log<T extends Length>(value: T) {
+  console.log(value, value.length);
   return value;
 }
-
-log<string[]>(["a", "b"]);
-log(["a", "b"]);
-
-// type Log = <T>(value: T) => T;
-// let myLog: Log = log;
-
-interface Log<T = string> {
-  <T>(value: T): T;
-}
-let myLog: Log = log;
-myLog("1");
+log([1]);
+log("123");
+log({ length: 1 });
